a9058ad0ce1bb88df13778bf0256f787aa404ecc
St Janne <jannis020609@gmail.com>
2020-02-20 16:27:04 +0100

    Update Program.cs

--- Online MMORPG/Program.cs
+++ Online MMORPG/Program.cs
@@ -16,9 +16,8 @@ namespace Online_MMORPG
             Console.WriteLine("type port");
             string portString = Console.ReadLine();
             bool success = int.TryParse(portString, out int port);
-            IPAddress ip = IPAddress.Parse("10.151.168.253");
-            
             TcpListener server = new TcpListener(IPAddress.Any, port);
+            Console.Clear();
             server.Start();
 
             while (true)
@@ -32,12 +31,9 @@ namespace Online_MMORPG
             
 
         }
-
-
         private static void SeperateThread(object obj)
         {
             var client = (TcpClient)obj;
-
             Byte[] bytes = new Byte[256];
             String data = null;
             NetworkStream stream = client.GetStream();
@@ -45,19 +41,26 @@ namespace Online_MMORPG
             messages.Add(stream);
             while (true)
             {
-                int i;
-
-                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                int i = 1;
+                while (i != 0)
                 {
-                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                    Console.WriteLine("Data: " + data);
-                    string response = data;
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(response);
-                    Loop(msg);
-
+                    try
+                    {
+                        i = stream.Read(bytes, 0, bytes.Length);
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Data: " + data);
+                        string response = data;
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(response);
+                        Loop(msg);
+                    }
+                    catch (Exception)
+                    {
+                        messages.Remove(stream);
+                        stream.Close();
+                        client.Close();
+                    }
                 }
                 client.Close();
-                
             }
 
         }
